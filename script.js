e99049a1c7285988c0345b37f9aff000f03a74f9
Luiz Almeida Jr <lcalmeidajr91@gmail.com>
2022-01-10 13:02:17 -0300

    styleModification
    Modificações em estilo e interações.

--- script.js
+++ script.js
@@ -1,60 +1,61 @@
-let num = document.getElementById("entrada")
-let lista = document.querySelector("select#lista")
-let res = document.getElementById("resultado")
-let dados = []
+var num = document.getElementById("entrada")
+dados = []
+var lista = document.getElementById("dados")
+var resultado = document.getElementById("resultado")
 
-function isNumber(n) {
-  if(Number(n) >= 1 && Number(n) <= 100) {
-    return true
-  } else {
-    return false
-  }
+function test1(n) {  // Avalia se o número está dentro do intervalo permitido
+    if(n >= 1 & n <= 100) {
+        return true
+    } else {
+        alert("Número fora do intervalo permitido!\nDigite um número de 1 a 100.")
+    }
 }
 
-function outList(n, l) {
-  if(l.indexOf(Number(n)) == -1) {
-    return true
-  } else {
-    return false
-  }
+function test2(n, valores) {  // Avalia se o número já foi adicionado anteriormente
+    if(valores.indexOf(n) == -1) {
+        return true
+    } else {
+        alert("Valor já adicionado!\nEscolha outro valor.")
+    }
 }
 
 function adicionar() {
-  if(isNumber(num.value) && outList(num.value, dados)) {
-    dados.push(Number(num.value))
-    let item = document.createElement("option")
-    item.text = `Valor ${num.value} adicionado.`
-    lista.appendChild(item)
-    res.innerHTML = ""
-  } else {
-    alert("Valor inválido ou já adicionado à lista!")
-  }
-  num.value = ""
-  num.focus()
+    if(test1(Number(num.value)) && test2(Number(num.value), dados)) {
+        dados.push(Number(num.value))
+        let item = document.createElement("option")
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+    }
+    num.value = ""
 }
 
 function finalizar() {
-  if(dados.length == 0) {
-    alert("Adicione valores antes de finalizar!")
-  } else {
-    let tot = dados.length
-    let maior = dados[0]
-    let menor = dados[0]
-    let soma = 0
-    let media = 0
-    for(let pos in dados) {
-      soma += dados[pos]
-      media += soma / tot
-      if(dados[pos] > maior)
-        maior = dados[pos]
-      if(dados[pos] < menor)
-        menor = dados[pos]
+    lista.innerHTML = ""
+    if(dados == "") {
+        alert("Adicione dados primeiro!")
+    } else {
+        let total = dados.length
+        let maior = dados[0]
+        let menor = dados [0]
+        let soma = 0
+        let media = 0
+        for(let i in dados) {
+            soma += dados[i]
+            media = soma / total
+            if(dados[i] > maior) {
+                maior = dados[i]
+            }
+            if(dados[i] < menor) {
+                menor = dados[i]
+            }
+        }
+        resultado.innerHTML = ""
+        resultado.innerHTML = `<p>Foram analisados ${total} números.</p>`
+        resultado.innerHTML += `<p>Os números analisados foram ${dados.join(", ")}.</p>`
+        resultado.innerHTML += `<p>O menor número analisado foi ${menor} e o maior deles foi ${maior}.</p>`
+        resultado.innerHTML += `<p>A soma dos números analisados foi ${soma} e sua média foi ${media.toFixed(2).replace(".", ",")}.`
+        alert(dados)
+        dados = ""
+        dados = []
     }
-    res.innerHTML = ""
-    res.innerHTML += `<p>Ao todo temos ${tot} números cadastrados.</p>`
-    res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
-    res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
-    res.innerHTML += `<p>A soma de todos os valores é ${soma}.</p>`
-    res.innerHTML += `<p>A média dos valores é ${media.toFixed(2).replace(".", ",")}.</p>`
-  }
 }
